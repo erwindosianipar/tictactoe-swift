fa034eb8f13ee4d1e993cbbe79a836a1956c5203
Erwindo Sianipar <erwindoq@gmail.com>
2020-03-02 17:47:10 +0700

    Initial Commit

---
+++ tictactoe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tictactoe
+//
+//  Created by macbook on 02/03/20.
+//  Copyright © 2020 macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
